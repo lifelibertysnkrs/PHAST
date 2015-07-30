54963c54fd1633aece680467b9c954d4798ccaf1
rahultiwarionline <rahultiwarionline@gmail.com>
2015-07-30 11:45:28 -0400

    Added focus code

---
+++ motorcontrol/motorcontrol.ino
@@ -0,0 +1,55 @@
+/*
+Developed at NASA Goddard
+ */
+
+// pins for the LEDs:
+const int input = 3;
+input motor = 7;
+int pos = 0;    // variable to store the servo position 
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  Servo myservo;  // create servo object to control a servo 
+                
+ 
+  
+  int increment = 15; //degrees to adjust focus by
+
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  while (Serial.available() > 0) {
+
+    // look for the next valid integer in the incoming serial stream:
+    int input = Serial.parseInt();
+
+    if (input == '0') {
+      pos = 0
+      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(1000)
+    }
+
+    if (input == '1'){
+      pos+= increment;
+      myservo.write(pos);
+      delay(1000)
+    }
+      
+       
+} 
+
+  
+
+}
+  
+
+
+
+
--- runphast.py
+++ runphast.py
@@ -4,10 +4,48 @@
 
  
 import subprocess
+import serial 
+def focusCam(ms, increments): #ms indicates shutter speed, increments indicates how many steps the motor takes to turn the focus all the way
+    focused = False; #Starts with assumption that camera is not focused
+    ser = serial.Serial('/dev/tty.usbserial', 9600) #Sets up Arduino
+    ser.write("0") #Causes Arduino to reset focus
+    while !focused && increments != 0:
+        #Takes picture
+        subprocess.check_output(["./TakePic/bin", str(ms)]) 
+        #Runs astrometry
+        subprocess.check_output(["solve-field", "/TakePic/pics/img-"+ str(ms) +".png", ">", "focus.txt"]) 
+        #If astrometry solved, returns true
+        focusfile = open("focus.txt", "r")
+        for line in focusfile:
+            if "solved with index" in line: focused = True
+        if !focused:
+            ser.write('1') #In arduino code, recieving 1 causes motor two turn focus by one increment
+            increment--
+    focusfile.close()
 
+    return focused
 
+def getLoc(ms)
+    subprocess.check_output(["./TakePic/bin", str(ms)])
 
-subprocess.check_output(["./TakePic/bin", '999'])
+    subprocess.check_output(["solve-field", "/TakePic/pics/img-"+ str(ms) +".png", ">", "results.txt"])
 
-#subprocess.check_output(["solve-field /TakePic/pics/img-1.png"])
+    retults = open("focus.txt", "r")
+    for line in results:
+            
+        if "Field center: (RA,Dec)" in line: return line
+        
+    focusfile.close()
+
+    return(1)
+
+def runPhast(ms, increment):
+    focused = focusCam(ms, increment)
+    loc = getLoc(ms)
+    
+    
+
+    
+
+    
 
